aece25d69450d8d788647c125ccdfd52626c8a84
dlfud <99ysl@naver.com>
2022-07-25 13:45:16 +0900

    학원 작업

--- src/main/java/com/sbs/project_instagram/board/controller/BoardController.java
+++ src/main/java/com/sbs/project_instagram/board/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.sbs.project_instagram.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +12,11 @@ public class BoardController {
     @RequestMapping("/list")
     public String list(){
         return "/board/list";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Long id){
+        return "board_detail";
     }
 
     @RequestMapping("/setting")
