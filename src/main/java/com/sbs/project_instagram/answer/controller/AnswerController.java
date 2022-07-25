35bfb81fe90152919626ee52bd1d23135489fbe5
dlfud <99ysl@naver.com>
2022-07-25 13:35:52 +0900

    학원 수정

--- src/main/java/com/sbs/project_instagram/answer/controller/AnswerController.java
+++ src/main/java/com/sbs/project_instagram/answer/controller/AnswerController.java
@@ -1,10 +1,17 @@
 package com.sbs.project_instagram.answer.controller;
 
+import com.sbs.project_instagram.answer.service.AnswerService;
+import com.sbs.project_instagram.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/answer")
+@RequiredArgsConstructor
 public class AnswerController {
+    private final BoardService boardService;
+    private final AnswerService answerService;
+
 
 }
